605a3a2d929fb370e3cf890bf5baf4514f15dfca
jordimall <jordi.mallafre@hotmail.com>
2023-12-19 12:42:17 +0100

    Test 5

--- GameLink2/src/main/java/com/example/demo/security/SimpleCORSFilter.java
+++ GameLink2/src/main/java/com/example/demo/security/SimpleCORSFilter.java
@@ -34,7 +34,7 @@ public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
     response.setHeader("Access-Control-Allow-Credentials", "true");
     response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
     response.setHeader("Access-Control-Max-Age", "3600");
-    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Host, Content-Lenght");
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Host, Content-Length");
 
     chain.doFilter(req, res);
 }
